7331a0ffaf04a866567f6cc24ae49bb45453f647
Tee Faizah <tee.faizah@gmail.com>
2025-05-21 18:49:56 +0600

    Add files via upload

---
+++ root/starter/basic.py
@@ -0,0 +1,45 @@
+# print("Hello") # syntax
+
+#comment
+
+# yguifed
+# hgbief
+# hfedgfi
+
+'''ygfdwe'''
+
+# variable naming
+
+'''
+1,2,3...
+@ % $
+_ can be used
+
+myVariable
+my_variable
+
+
+list1
+list_2
+'''
+# indentation
+
+# if 5 > 2:
+#  print('Five is greater than two!')   
+
+# variable 
+
+# number_1 = 5
+# number2 = 4
+# number3 = 3
+# number4 = 2
+
+# print(number_1 + number2)
+
+
+# data type
+
+# variable = data
+x = 4     #--> integer
+y = 'Sally'    #--> string
+z = 3.5      #--> float   
---
+++ root/starter/data_type.py
@@ -0,0 +1,21 @@
+'''
+new document . doc
+file name.xlsx
+file .txt
+.cpp
+.c
+'''
+
+
+''' DATA TYPE
+
+Text type = str
+Numeric = int, float
+sequence = list, range, tuple
+Mapping = dictionary
+Boolean = True False 
+'''
+
+x = input()
+y = "Sally"
+print(type(x))
